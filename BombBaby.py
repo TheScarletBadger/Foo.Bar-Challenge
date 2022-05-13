b9790d3d616243d1fab5a015654bc092ebc352ce
TheScarletBadger <56405836+TheScarletBadger@users.noreply.github.com>
2022-05-13 11:30:15 +0100

    Add files via upload

---
+++ BombBaby.py
@@ -0,0 +1,46 @@
+#final version
+
+def loopyloo(z):
+    loop = 0
+    while max(z)%min(z)!=0:
+        big,small = max(z),min(z)
+        loop = loop+(big//small)
+        big = big%small
+        if z[0]>z[1]:
+            z=(big,small)
+        else:
+            z=(small,big)
+        
+    while z!=(1,1):
+        big,small = max(z),min(z)
+        if(sum(z)<=0) or big==small:
+            return 'impossible'
+        
+        if small==1:
+            loop = loop+big-1
+            return str(loop)
+
+        if z[0]>z[1]:
+            z=(z[0]-z[1],z[1])
+        else:
+            z=(z[0],z[1]-z[0])
+        loop = loop+1
+    return str(loop)
+
+def solution(x,y):
+    x=int(x)
+    y=int(y)
+    target = (x,y)
+
+    #use shortcuts if possible
+    if x==y:
+        return 'impossible'
+    if sum([x,y])>3:
+        if x%y==0 or y%x==0:
+            return 'impossible'
+
+    return loopyloo(target)
+
+#print(solution('3000000000000000000000000000000000000001',str(10**50)))
+print(solution('7','4'))
+
---
+++ BunnyKeyEscape(final).py
@@ -0,0 +1,25 @@
+#Brute force and ignorance?
+from itertools import combinations as combi
+
+def solution(n,k):
+    #The cap on number of each key type (kcap) in circulation is given by n-(k-1)
+    kcap = n-(k-1)
+    
+    #lets make a matrix giving all posible ways of sampling kcap from a vector of length n.
+    subsets = list(combi(list(range(0,n)),kcap))
+    
+    #initialize an empty matrix to hold the key distribution
+    scheme = [[] for x in range(0,n)]
+    
+    #in 'subsets' row indices are key type names and elements are rabbit names
+    #so we just need todo the old switcheroo
+    
+    for r in range(0,len(subsets)):
+        subsrow = subsets[r]
+        for c in range(0,len(subsrow)):
+            rabbit = subsets[r][c]
+            keytype = r
+            scheme[rabbit].append(keytype)
+
+    return scheme
+
