894271c86c25587a78cb8eb7c526baf63b6898f7
TheScarletBadger <56405836+TheScarletBadger@users.noreply.github.com>
2022-05-22 21:23:07 +0100

    Update and rename running-with-the-bunnies(final).py to RunningWithTheBunniesFinal.py

--- RunningWithTheBunniesFinal.py
+++ RunningWithTheBunniesFinal.py
@@ -1,5 +1,9 @@
 import itertools as it
 loopalert=0
+
+#Here I use a slightly wonky implementation of Djikstras algorithm to convert a row of the
+#original transition cost matrix into a new one which indicates the shortest route
+#between rows (even if its a multi-hop)
 def djikrow(start,mat):    
     global loopalert
     q = []
@@ -21,8 +25,9 @@ def djikrow(start,mat):
         nidx = [n for n in range(len(mat)) if n != cpos]
         for n in nidx:
             ndis = cdis+cost[n]
-            #best guess is that if we reach a value of -1000 theres
-            #a loop in graph allowing infinite time refils
+            #Something I didn't account for was the possibility that the original graph contains
+            #a negative cost loop allowing infinite time refils. This is my crude method of detecting
+            #one and if one is detected simply reporting that we can rescue all bunnies.
             if ndis <-1000:
                 loopalert = 1
                 return
@@ -34,6 +39,7 @@ def djikrow(start,mat):
     out = [d[x] for x in range(len(d))]
     return out
 
+#function uses the one above and iterates over rows to create an updated transition cost matrix
 def updatemat(mat):
     newmat = []
     for row in range(len(mat)):
@@ -42,8 +48,9 @@ def updatemat(mat):
             return
     return newmat
 
+#calculate all permutations of all subsets of rows containing rabbits. These are the
+#various groups we can attempt a rescue and the orders we can try visiting them in
 def getopts(mat):
-    #calculate all permutations of all subsets of rows containing rabbits
     options = []
     for sub in range(1,len(mat)-1):
         ss = it.combinations(list(range(1,len(mat)-1)), sub)
@@ -51,7 +58,6 @@ def getopts(mat):
             p = list(it.permutations(s))
             for p2 in p:
                 options.append(list(p2))
-                #options.insert(0,list(p2))
     return options
 
 def solution(mat,t):
